f698286d7e5aafd13a39010991600acf7879b074
Przemo Firszt <przemo@firszt.eu>
2014-07-11 20:37:38 +0100

    Change test render to animation with clean background
    Signed-off-by: Przemo Firszt <przemo@firszt.eu>

--- src/occ.py
+++ src/occ.py
@@ -16,26 +16,25 @@ class open_cycle_computer():
 		self.width = width
 		self.height = height
 		self.screen = pygame.display.set_mode((self.width, self.height))
+		self.clock = pygame.time.Clock()
 		self.fg_colour = 255, 255, 255
-		self.bg_colour = 5, 5, 5
+		#self.bg_colour = 5, 5, 5
 		self.win_color = 0, 0, 0
-		self.screen.fill(self.win_color)
-		self.draw_frame()
 
 	def main_loop(self):
 		while 1:
 			for event in pygame.event.get():
 				if event.type in (QUIT, KEYDOWN, MOUSEBUTTONDOWN):
 					sys.exit()
+			t = pygame.time.get_ticks()/1000
+			self.screen.fill(self.win_color)
+			self.render_top(t)
+			self.render_mid(50 + 10 * t)
+			self.render_bl(t)
+			self.render_br(t)
+			self.draw_frame()
+			self.clock.tick(100)
 			pygame.display.flip()
-			for i in range(100):
-				self.render_top(i)
-			for i in range(1000):
-				self.render_mid(i)
-			for i in range(100):
-				self.render_bl(i)
-			for i in range(100):
-				self.render_br(i)
 
 	def render_value(self, value, position, size):
 		font = pygame.font.Font(None, 12 * size)
@@ -64,7 +63,6 @@ class open_cycle_computer():
 		pygame.draw.line(self.screen, (220, 220, 220), (0, 240), (240, 240), 1)
 		pygame.draw.line(self.screen, (220, 220, 220), (self.width/2, 240), (self.width/2, self.height), 1)
 		self.draw_speed_unit()
-
 
 
 
