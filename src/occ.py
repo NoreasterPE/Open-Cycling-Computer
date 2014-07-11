e96eeb4cb10a0982e4e38bde9f1b1876c987ca0c
Przemo Firszt <przemo@firszt.eu>
2014-07-11 20:10:37 +0100

    Add top and middle window rendering
    Signed-off-by: Przemo Firszt <przemo@firszt.eu>

--- src/occ.py
+++ src/occ.py
@@ -29,30 +29,34 @@ class open_cycle_computer():
 			#	if event.type == pygame.QUIT: 
 			if pygame.event.wait().type in (QUIT, KEYDOWN, MOUSEBUTTONDOWN):
 				sys.exit()
+			pygame.display.flip()
+
+	def render_value(self, value, position, size):
+		font = pygame.font.Font(None, 12 * size)
+		ren = font.render(value, 1, self.fg_colour)
+		x = ren.get_rect().centerx
+		y = ren.get_rect().centery
+		self.screen.blit(ren, (position[0] - x, position[1] - y))
+
+	def render_top(self, value):
+		self.render_value(str(value), (120 - 20, 75), 18)
+
+	def render_mid(self, value):
+		self.render_value(str(value), (120, 195), 10)
 
 	def draw_text(self):
-		font = pygame.font.Font(None, 18 * 12)
-		text = '48'
-		font.set_bold(0)
-
-		ren = font.render(text, 1, self.fg_colour)
-		self.screen.blit(ren, (10, 0))
-
-		font = pygame.font.Font(None, 3 * 12)
-		unit = 'km/h'
-		ren = font.render(unit, 1, self.fg_colour)
-		self.screen.blit(ren, (180, 115))
-
-		font = pygame.font.Font(None, 8 * 12)
-		unit = '165'
-		ren = font.render(unit, 1, self.fg_colour)
-		self.screen.blit(ren, (10, 180))
-		pygame.display.flip()
+		for i in range(100):
+			self.render_top(i)
+		for i in range(1000):
+			self.render_mid(i)
 
 	def draw_frame(self):
 		pygame.draw.line(self.screen, (220, 220, 220), (0, 150), (self.width, 150), 1)
 		pygame.draw.line(self.screen, (220, 220, 220), (0, 240), (240, 240), 1)
 		pygame.draw.line(self.screen, (220, 220, 220), (self.width/2, 240), (self.width/2, self.height), 1)
+
+		self.render_value("km/h", (210, 75), 3)
+
 
 
 
