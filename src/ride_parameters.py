e47355c2276a2febc8f04978456842e886e02164
Przemo Firszt <przemo@firszt.eu>
2014-08-03 21:50:29 +0000

    Move more parameters to ride_parameters

--- src/ride_parameters.py
+++ src/ride_parameters.py
@@ -4,3 +4,9 @@ class ride_parameters():
         def __init__(self):
                 self.speed = 0
 		self.speed_tenths = math.floor (10 * (self.speed - math.floor(self.speed)))
+		self.heart_rate = 165
+		self.heart_rate_units = "BPM"
+		self.gradient = 10
+		self.gradient_units = "%"
+		self.cadence = 109
+		self.units = "km/h"
