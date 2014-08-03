717b4fe70d0b90ba4fd6330d504335c3e697e329
Przemo Firszt <przemo@firszt.eu>
2014-08-03 22:24:42 +0000

    Add multiple page handling in layout

--- src/layout.py
+++ src/layout.py
@@ -4,6 +4,7 @@ import xml.etree.ElementTree as eltree
 
 class layout():
 	def __init__(self, xml_file):
+		self.page_list = {}
 		self.layout_path = xml_file
 		self.load_layout()
 		self.name = None
@@ -18,20 +19,27 @@ class layout():
 		#	print "font size : " + field.find('font_size').text
 	def load_layout(self):
 		layout_tree = eltree.parse(self.layout_path)
-		self.page = layout_tree.getroot()
-		self.bg_image = pygame.image.load(self.page.get('background')).convert() 
-		self.name = self.page.get('name') 
-		self.font = self.page.get('font') 
+		self.pages = layout_tree.getroot()
+		for page in self.pages:
+			#print "page name : ", page.get('name')
+			self.page_list[page.get('name')] = page
+			
+		self.use_page()
+
+	def use_page(self, page_name = "Main"):
+		self.current_page = self.page_list[page_name]
+		self.bg_image = pygame.image.load(self.current_page.get('background')).convert() 
+		self.font = self.current_page.get('font') 
 		if (self.font == ""):
 			self.font = None
-		self.fg_colour_rgb = self.page.get('fg_colour') 
+		self.fg_colour_rgb = self.current_page.get('fg_colour') 
 		self.fg_colour = struct.unpack('BBB',self.fg_colour_rgb.decode('hex'))
 
 	def render_background(self, screen):
 		screen.blit(self.bg_image, [0, 0])
 
 	def render(self, screen, function, value):
-		for field in self.page:
+		for field in self.current_page:
 			if (field.find('function').text == function):
 				font = pygame.font.Font(self.font, 12 * int(field.find('font_size').text))
 				ren = font.render(str(value), 1, self.fg_colour)
