8fc436c1134792de59e3a68b6bb0594690af93dd
Przemo Firszt <przemo@firszt.eu>
2014-08-29 18:31:29 +0000

    Make sure measurement is stopped on exit
    Signed-off-by: Przemo Firszt <przemo@firszt.eu>

--- src/bmp183_test.py
+++ src/bmp183_test.py
@@ -1,15 +1,20 @@
 import time
 from bmp183 import bmp183
-
+#Init sensor
 bmp = bmp183()
+#Start measuring
 bmp.start()
+#Run printing out results for 60 seconds, every second
 run = 0
-
-while (run != 60):
-	print "Temperature: ", bmp.temperature, "deg C"
-	print "Pressure: ", bmp.pressure/100.0, " hPa"
-	time.sleep(1)
-	run += 1
-
-bmp.stop_measurement()
+try:
+	while (run != 60):
+		print "Temperature: ", bmp.temperature, " deg C"
+		print "Pressure: ", bmp.pressure/100.0, " hPa"
+		time.sleep(1)
+		run += 1
+	#Stop measuring
+	bmp.stop_measurement()
+except:
+	#Stop measuring after an error or Ctrl-C
+	bmp.stop_measurement()
 quit()
