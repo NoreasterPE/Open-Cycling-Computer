d3ea03ad378e241df8a2170c6d575255e9fe81aa
Przemo Firszt <przemo@firszt.eu>
2014-09-26 18:42:22 +0000

    Make usable gps_mkt3339 module
    Signed-off-by: Przemo Firszt <przemo@firszt.eu>

---
+++ src/gps_mtk339.py
@@ -0,0 +1,48 @@
+#! /usr/bin/python
+ 
+from gps import *
+from time import *
+import os
+import threading
+import time
+ 
+class gps_mtk3339(threading.Thread):
+	def __init__(self):
+		threading.Thread.__init__(self)
+		self.data = gps(mode=WATCH_ENABLE | WATCH_NEWSTYLE)
+		self.running = False
+	def run(self):
+		self.running = True
+		while self.running:
+			self.data.next()
+	def stop(self):
+		self.running = False
+ 
+if __name__ == '__main__':
+	gps = gps_mtk3339()
+	try:
+		gps.start()
+		while True:
+			os.system('clear')
+
+			print 'latitude    ' , gps.data.fix.latitude
+			print 'longitude   ' , gps.data.fix.longitude
+			print 'time utc    ' , gps.data.utc
+			print 'fix time    ' , gps.data.fix.time
+			print 'altitude (m)' , gps.data.fix.altitude
+			print 'eps         ' , gps.data.fix.eps
+			print 'epx         ' , gps.data.fix.epx
+			print 'epv         ' , gps.data.fix.epv
+			print 'ept         ' , gps.data.fix.ept
+			print 'speed (m/s) ' , gps.data.fix.speed
+			print 'climb       ' , gps.data.fix.climb
+			print 'track       ' , gps.data.fix.track
+			print 'mode        ' , gps.data.fix.mode
+			print 'sats        ' , gps.data.satellites
+
+			time.sleep(1)
+ 
+	except (KeyboardInterrupt, SystemExit):
+	#FIXME possibly required on shutdown
+		gps.stop()
+		gps.join()
