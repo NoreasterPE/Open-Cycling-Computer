a0397bd74e9f95f3edf3e0c5455526defa4dae52
Przemo Firszt <przemo@firszt.eu>
2014-10-16 11:22:24 +0000

    Fix resetting layout_changed variable
    Signed-off-by: Przemo Firszt <przemo@firszt.eu>

--- src/occ.py
+++ src/occ.py
@@ -111,7 +111,7 @@ class open_cycle_computer():
 
 			if self.rp.params_changed or self.layout.layout_changed:
 				self.rp.params_changed = 0
-				self.layout_changed = 0
+				self.layout.layout_changed = 0
 				self.layout.render_page(self.rp)
 			#print self.clock.get_fps()
 			#Setting FPS too low causes some click-directly-after-click problems
