e0c47b6d2a44b6b8dd723bd5985fea5b9a6d65d2
Przemo Firszt <przemo@firszt.eu>
2014-11-26 18:46:17 +0000

    [UN] Make function names more consistent
    Signed-off-by: Przemo Firszt <przemo@firszt.eu>

--- src/units.py
+++ src/units.py
@@ -1,57 +1,62 @@
 #! /usr/bin/python
-#FIXME consistent function names
 
 class units():
 	def __init__(self):
 		pass
 
 	def convert(self, value, target_unit):
-		conversions = { "F" : self.temp_to_F(value),
-		                "K" : self.temp_to_K(value),
-		                "kmh" : self.ms_to_kmh(value),
-		                "mph" : self.ms_to_mph(value),
-		                "pd" : self.kg_to_pd(value),
-		                "st_lb" : self.kg_to_st_lb(value),
-		                "km" : self.m_to_km(value),
-		                "mi" : self.m_to_mi(value),
-		                "yd" : self.m_to_yd(value),
+		conversions = { "F" : self.temp_C_to_F(value),
+		                "K" : self.temp_C_to_K(value),
+		                "km/h" : self.speed_ms_to_kmh(value),
+		                "mi/h" : self.speed_ms_to_mph(value),
+		                "pd" : self.mass_kg_to_pd(value),
+		                "st_lb" : self.mass_kg_to_st_lb(value),
+		                "km" : self.dist_m_to_km(value),
+		                "mi" : self.dist_m_to_mi(value),
+		                "yd" : self.dist_m_to_yd(value),
+		                "hPa" : value,
+		                "kg" : value,
+		                "s" : value,
+		                "m/s" : value,
+		                "m" : value,
+		                "" : value,
 		}
 		return conversions[target_unit]
 
-	def temp_to_F(self, temp):
+	def temp_C_to_F(self, temp):
 		tF = temp * 1.8 + 32
 		return tF
 
-	def temp_to_K(self, temp):
+	def temp_C_to_K(self, temp):
 		tK = 273.15 + temp
 		return tK
 
-	def ms_to_kmh(self, speed):
+	def speed_ms_to_kmh(self, speed):
 		s_kmh = speed * 3.6
 		return s_kmh
 
-	def ms_to_mph(self, speed):
+	def speed_ms_to_mph(self, speed):
 		s_mph = speed * 2.23694
 		return s_mph
 
-	def kg_to_pd(self, mass):
+	def mass_kg_to_pd(self, mass):
 		m_pd = mass / 0.45359237
 		return m_pd
 
-	def kg_to_st_lb(self, mass):
+	def mass_kg_to_st_lb(self, mass):
 		m_st = int(mass * 0.15747)
 		m_lb = (mass - (m_st / 0.15747)) * 2.2046
 		return (m_st, m_lb)
 
-	def m_to_km(self, dist):
+	def dist_m_to_km(self, dist):
 		d_km = dist / 1000
 		return d_km
 
-	def m_to_mi(self, dist):
+	def dist_m_to_mi(self, dist):
 		d_mi = dist / 1609.344
 		return d_mi
 
-	def m_to_yd(self, dist):
+	def dist_m_to_yd(self, dist):
 		d_yd = dist / 0.9144
 		return d_yd
 
