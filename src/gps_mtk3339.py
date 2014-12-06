7367e7a1122023c21ffb57d77ef2f6dae8a870e7
Przemo Firszt <przemo@firszt.eu>
2014-12-06 16:11:13 +0000

    [GPS] Add missing self.fix_time conversion (how it was removed?)
    Signed-off-by: Przemo Firszt <przemo@firszt.eu>

--- src/gps_mtk3339.py
+++ src/gps_mtk3339.py
@@ -63,6 +63,7 @@ class gps_mtk3339(threading.Thread):
 					try:
 						#FIXME Fails sometimes with ImportError from gps.py - see TODO 21 [IN TESTING]
 						self.data.next()
+						data = self.data
 						gps_data_available = True
 					except StopIteration:
 						self.l.error("[GPS] StopIteration exception in GPS")
@@ -71,31 +72,23 @@ class gps_mtk3339(threading.Thread):
 						time.sleep(0.5)
 						pass
 					if gps_data_available:
-						self.latitude = self.data.fix.latitude
-						self.longitude = self.data.fix.longitude
-						self.utc = self.data.utc
-						self.climb = self.data.fix.climb
-						self.speed = self.data.fix.speed
-						self.altitude = self.data.fix.altitude
-						self.fix_mode = fix_mode[self.data.fix.mode]
-						self.fix_time = self.data.fix.time
+						self.latitude = data.fix.latitude
+						self.longitude = data.fix.longitude
+						self.utc = data.utc
+						self.climb = data.fix.climb
+						self.speed = data.fix.speed
+						self.altitude = data.fix.altitude
+						self.fix_mode = fix_mode[data.fix.mode]
+						self.fix_time = data.fix.time
 						if not isinstance(self.fix_time, float):
 							#Workaround for python bug
-							#ImportError: Failed to import _strptime because the import lockis held by another thread.
+							#ImportError: Failed to import _strptime because the import lock is held by another thread.
 							try:
 								#self.data.fix.time is a string, so parse it to get the float time value.
-								self.fix_time = time.mktime(time.strptime(self.data.fix.time, '%Y-%m-%dT%H:%M:%S.%fZ'))
+								self.fix_time = time.mktime(time.strptime(data.fix.time, '%Y-%m-%dT%H:%M:%S.%fZ'))
 							except ImportError:
 								self.l.critical("[GPS] self.fix_time {}".format(self.fix_time))
 								pass
-#FIXME
-#Exception in thread Thread-1:
-#Traceback (most recent call last):
-#  File "/usr/lib/python2.7/threading.py", line 552, in __bootstrap_inner
-#    self.run()
-#  File "/home/pi/OpenCyclingComputer/src/gps_mtk3339.py", line 84, in run
-#    self.lag = timestamp - self.fix_time
-#TypeError: unsupported operand type(s) for -: 'float' and 'unicode'
 						self.lag = timestamp - self.fix_time
 						self.l.debug("[GPS] timestamp to fix time delta: {}".format(self.lag))
 						if self.set_time:
