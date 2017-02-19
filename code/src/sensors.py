6efc9594ab707457a4e4dfedd55326b0007023bc
Przemo Firszt <przemo@firszt.eu>
2017-02-19 10:39:07 +0000

    Improve stopping and make preps for more sensors
    Signed-off-by: Przemo Firszt <przemo@firszt.eu>

--- code/src/sensors.py
+++ code/src/sensors.py
@@ -16,26 +16,28 @@ class sensors(threading.Thread):
     def __init__(self, simulate=False):
         threading.Thread.__init__(self)
         self.l = logging.getLogger('system')
+        self.sensors = dict(ble=None)
         self.simulate = simulate
         self.connected = {'ble': False}
+        self.running = True
 
     def run(self):
         if not self.simulate:
-            while not self.connected['ble']:
+            while not self.connected['ble'] and self.running:
                 self.l.info("[RP] Initialising BLE sensor")
                 #FIXME Hardcoded address
                 try:
-                    self.ble = ble(self.simulate, "fd:df:0e:4e:76:cf")
+                    self.sensors['ble'] = ble(self.simulate, "fd:df:0e:4e:76:cf")
                     self.l.info("[RP] Starting BLE thread")
-                    self.ble.start()
+                    self.sensors['ble'].start()
                     self.connected['ble'] = True
                 except BTLEException:
                     self.l.info("[RP] Connecion to BLE sensor failed")
 
     def get_sensor(self, name):
         #FIXME Currnetly only ble
-        if self.connected[name]:
-            return self.ble
+        if name in self.sensors and self.connected[name]:
+            return self.sensors[name]
         else:
             return None
 
@@ -44,5 +46,8 @@ class sensors(threading.Thread):
 
     def stop(self):
         self.connected['ble'] = False
+        self.running = False
         self.l.info("[RP] Stopping BLE thread")
-        self.ble.stop()
+        if self.sensors['ble']:
+            self.sensors['ble'].stop()
+        self.sensors['ble'] = None
