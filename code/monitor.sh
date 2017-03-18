a9c09e8b03214a955cd2f79ff7312aae9bbc99b6
Przemo Firszt <przemo@firszt.eu>
2017-03-18 09:09:04 +0000

    Move config handling to a separate module
    Signed-off-by: Przemo Firszt <przemo@firszt.eu>

--- code/monitor.sh
+++ code/monitor.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 LAST_LOG=$(ls -1r log/debug.* | head -n1)
-echo Last log $LAST_LOG
+echo $LAST_LOG
 head -n1 $LAST_LOG && tail -f $LAST_LOG
--- code/src/occ.py
+++ code/src/occ.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python
 
 from layout import layout
+from occ_config import occ_config
 from operator import add
 from pygame.locals import NOEVENT
 from pygame.locals import USEREVENT
 from rendering import rendering
 from ride_parameters import ride_parameters
 from sensors import sensors
-from shutil import copyfile
 from time import sleep
 from time import strftime
-from wheel import wheel
 import logging
 import logging.handlers
-import lxml.etree as eltree
 import os
 import platform
 import pygame
@@ -35,7 +33,7 @@ REFRESH_TIME = 1000
 CONFIG_SAVE_TIME = 15000
 
 
-class open_cycling_computer():
+class open_cycling_computer(object):
     'Class for PiTFT 2.8" 320x240 cycling computer'
 
     def __init__(self, simulate=False, width=240, height=320):
@@ -59,10 +57,9 @@ class open_cycling_computer():
         self.sensors = sensors(self, simulate)
         self.l.debug("[OCC] Calling ride_parameters")
         self.rp = ride_parameters(self, simulate)
-        self.config_path = "config/config.xml"
-        self.l.debug("[OCC] Reading config. Path = {}".format(self.config_path))
-        self.read_config()
-        self.l.debug("[OCC] Setting layout. Path = {}".format(self.layout_path))
+        self.layout_path = ''
+        self.config = occ_config(self, "config/config.xml", "config/config_base.xml")
+        self.config.read_config()
         self.layout = layout(self, self.layout_path)
         self.l.debug("[OCC] Starting RP sensors")
         self.rp.start_sensors()
@@ -73,9 +70,7 @@ class open_cycling_computer():
         self.released_t = 0
         self.rel_movement = 0
         self.pressed_t = 0
-        self.config = 0
         self.pressed_pos = 0
-        self.layout_path = 0
         self.released_pos = 0
         self.add_rel_motion = 0
         self.running = True
@@ -94,133 +89,9 @@ class open_cycling_computer():
     def force_refresh(self):
         self.refresh = True
 
-    def read_config(self):
-        self.l.debug("[OCC][F] read_config")
-        try:
-            config_tree = eltree.parse(self.config_path)
-        except IOError:
-            self.l.exception("[OCC] I/O Error when trying to parse config file. Making copy of base_config")
-            copyfile('config/config_base.xml', 'config/config.xml')
-            self.config_path = "config/config.xml"
-            try:
-                config_tree = eltree.parse(self.config_path)
-            except IOError:
-                self.l.exception("[OCC] I/O Error when trying to parse to default config. Quitting!!")
-                self.cleanup()
-        self.config = config_tree.getroot()
-        try:
-            log_level = self.config.find("log_level").text
-            self.switch_log_level(log_level)
-            self.rp.params["debug_level"] = log_level
-        except AttributeError:
-            pass
-        try:
-            self.layout_path = self.config.find("layout_path").text
-        except AttributeError:
-            self.layout_path = "layouts/default.xml"
-            self.l.error("[OCC] Missing layout path, falling back to default.xml")
-        error_list = []
-        try:
-            self.rp.p_raw["riderweight"] = float(self.config.find("riderweight").text)
-        except AttributeError:
-            error_list.append("riderweight")
-        try:
-            self.rp.p_raw["wheel_size"] = self.config.find("wheel_size").text
-            self.rp.params["wheel_size"] = self.rp.p_raw["wheel_size"]
-            w = wheel()
-            try:
-                self.rp.p_raw["wheel_circ"] = w.get_size(self.rp.p_raw["wheel_size"])
-            except KeyError:
-                error_list.append("wheel_circ")
-            self.rp.params["wheel_circ"] = self.rp.p_raw["wheel_circ"]
-        except AttributeError:
-            error_list.append("wheel_size")
-        try:
-            self.rp.units["riderweight"] = self.config.find("riderweight_units").text
-        except AttributeError:
-            error_list.append("riderweight_units")
-        try:
-            self.rp.p_raw["altitude_home"] = float(self.config.find("altitude_home").text)
-        except AttributeError:
-            error_list.append("")
-        try:
-            self.rp.units["altitude_home"] = self.config.find("altitude_home_units").text
-        except AttributeError:
-            error_list.append("altitude_home")
-        try:
-            self.rp.p_raw["odometer"] = float(self.config.find("odometer").text)
-        except AttributeError:
-            error_list.append("odometer")
-        try:
-            self.rp.units["odometer"] = self.config.find("odometer_units").text
-        except AttributeError:
-            error_list.append("odometer")
-        try:
-            self.rp.p_raw["ridetime_total"] = float(self.config.find("ridetime_total").text)
-        except AttributeError:
-            error_list.append("ridetime_total")
-        try:
-            self.rp.p_raw["speed_max"] = float(self.config.find("speed_max").text)
-        except AttributeError:
-            error_list.append("speed_max")
-        try:
-            self.rp.units["speed"] = self.config.find("speed_units").text
-        except AttributeError:
-            error_list.append("speed")
-        try:
-            self.rp.units["temperature"] = self.config.find("temperature_units").text
-        except AttributeError:
-            error_list.append("temperature")
-        try:
-            self.rp.p_raw["ble_hr_name"] = self.config.find("ble_hr_name").text
-        except AttributeError:
-            error_list.append("ble_hr_name")
-        try:
-            self.rp.p_raw["ble_hr_addr"] = self.config.find("ble_hr_addr").text
-        except AttributeError:
-            error_list.append("ble_hr_addr")
-        try:
-            self.rp.p_raw["ble_sc_name"] = self.config.find("ble_sc_name").text
-        except AttributeError:
-            error_list.append("ble_sc_name")
-        try:
-            self.rp.p_raw["ble_sc_addr"] = self.config.find("ble_sc_addr").text
-        except AttributeError:
-            error_list.append("ble_sc_addr")
-        self.rp.update_param("speed_max")
-        self.rp.split_speed("speed_max")
-        if len(error_list) > 0:
-            for item in error_list:
-                self.l.error("[OCC] Missing: {} in config file".format(item))
-            error_list = []
-
     def switch_log_level(self, log_level):
         self.l.setLevel(LOG_LEVEL[log_level])
         self.l.log(100, "[OCC] Switching to log_level {}".format(log_level))
-
-    def write_config(self):
-        self.l.debug("[OCC] Writing config file")
-        log_level = logging.getLevelName(self.l.getEffectiveLevel())
-        config_tree = eltree.Element("config")
-        eltree.SubElement(config_tree, "log_level").text = log_level
-        eltree.SubElement(config_tree, "layout_path").text = self.layout.layout_path
-        eltree.SubElement(config_tree, "riderweight").text = unicode(self.rp.p_raw["riderweight"])
-        eltree.SubElement(config_tree, "riderweight_units").text = unicode(self.rp.units["riderweight"])
-        eltree.SubElement(config_tree, "wheel_size").text = self.rp.p_raw["wheel_size"]
-        eltree.SubElement(config_tree, "altitude_home").text = unicode(self.rp.p_raw["altitude_home"])
-        eltree.SubElement(config_tree, "altitude_home_units").text = unicode(self.rp.units["altitude_home"])
-        eltree.SubElement(config_tree, "odometer").text = unicode(self.rp.p_raw["odometer"])
-        eltree.SubElement(config_tree, "odometer_units").text = unicode(self.rp.units["odometer"])
-        eltree.SubElement(config_tree, "ridetime_total").text = unicode(self.rp.p_raw["ridetime_total"])
-        eltree.SubElement(config_tree, "speed_max").text = unicode(self.rp.p_raw["speed_max"])
-        eltree.SubElement(config_tree, "speed_units").text = unicode(self.rp.units["speed"])
-        eltree.SubElement(config_tree, "temperature_units").text = unicode(self.rp.units["temperature"])
-        eltree.SubElement(config_tree, "ble_hr_name").text = self.rp.p_raw["ble_hr_name"]
-        eltree.SubElement(config_tree, "ble_hr_addr").text = self.rp.p_raw["ble_hr_addr"]
-        eltree.SubElement(config_tree, "ble_sc_name").text = self.rp.p_raw["ble_sc_name"]
-        eltree.SubElement(config_tree, "ble_sc_addr").text = self.rp.p_raw["ble_sc_addr"]
-        # FIXME error handling for file operation
-        eltree.ElementTree(config_tree).write(self.config_path, encoding="UTF-8", pretty_print=True)
 
     def screen_touched_handler(self, time_now):
         if (time_now - self.pressed_t) > LONG_CLICK:
@@ -265,7 +136,7 @@ class open_cycling_computer():
             self.rp.update_values()
         elif event.type == EV_SAVE_CONFIG:
             self.l.debug("[OCC] calling write_config {}".format(time_now))
-            self.write_config()
+            self.config.write_config()
         elif event.type == pygame.MOUSEBUTTONDOWN:
             self.pressed_t = time_now
             self.pressed_pos = pygame.mouse.get_pos()
