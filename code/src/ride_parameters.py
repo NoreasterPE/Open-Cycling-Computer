9263be3e36bf7e59253a1662be233dd0237be4c0
Przemo Firszt <przemo@firszt.eu>
2018-10-15 16:11:05 +0100

    Clean ride_parameters
    Signed-off-by: Przemo Firszt <przemo@firszt.eu>

--- code/src/ride_parameters.py
+++ code/src/ride_parameters.py
@@ -48,49 +48,17 @@ class ride_parameters():
 
         self.suffixes = ("_hms")
 
-        self.p_raw = dict(time_stamp=time.time(),
-                          # Time delta since last p_raw update
-                          time_delta=1, time_adjustment_delta=0.0,
-                          ride_time_reset=0.0001,
-                          ride_time=0.0, ride_time_total=0.0,
-                          speed_low=1.0,
-                          timeon=0.0001, utc='', rtc='')
-
-        # Internal units
-        self.p_raw_units = dict(
-            time_delta='s',
-            ride_time='s', ride_time_total='s', timeon='s', ride_time_reset='s')
-
         # Params of the ride ready for rendering.
         self.params = dict(
-            time_delta=0,
-            ride_time='', ride_time_hms='', ride_time_total='',
-            ride_time_total_hms='', rtc='',
-            timeon='', timeon_hms='', ride_time_reset='', utc='',
             # Editor params
             editor_index=0, variable=None,
             variable_description=None, variable_raw_value=None, variable_unit=None, variable_value=None)
-
-        # Formatting strings for params.
-        self.p_format = dict(
-            time_delta='%.2f',
-            ride_time='%.0f', ride_time_hms='', ride_time_total='.0f',
-            ride_time_total_hms='', rtc='',
-            timeon='%.0f', timeon_hms='', ride_time_reset='%.0f', utc='')
-
-        # Units - name has to be identical as in params
-        # FIXME rename to p_units for consistency
-        self.p_units = dict(
-            time_delta='s',
-            ride_time='s', ride_time_hms='', ride_time_total='s', ride_time_total_hms='',
-            timeon='s', timeon_hms='', ride_time_reset='s')
 
         self.log.debug("Setting up event scheduler", extra=self.extra)
         self.event_scheduler.enter(RIDE_PARAMETERS_UPDATE, 1, self.schedule_update_event)
 
     def schedule_update_event(self):
         self.log.debug("Calling update values from event scheduler", extra=self.extra)
-        self.update_values()
         self.ride_log.add_entry(self.params)
         if not self.stopping:
             #Setting up next update event
@@ -107,28 +75,6 @@ class ride_parameters():
 
     def __del__(self):
         self.stop()
-
-    def update_values(self):
-        t = time.time()
-        self.p_raw["time_delta"] = t - self.p_raw["time_stamp"]
-        self.p_raw["time_stamp"] = t
-        #FIXME Time adjustment
-        #dt_adjustment = self.p_raw['time_adjustment_delta']
-        #if dt_adjustment > 0:
-        #    self.p_raw["time_delta"] = self.p_raw["time_delta"] - dt_adjustment
-        #    self.log.info("time_delta adjusted by {}".format(dt_adjustment), extra=self.extra)
-        #    self.sensors['gps'].time_adjustment_delta = 0
-        #    # FIXME Correct other parameters like ride_time
-        #self.log.debug("timestamp: {} time_delta {:10.3f}".format(time.strftime("%H:%M:%S", time.localtime(t)), self.p_raw["time_delta"]), extra=self.extra)
-
-        self.calculate_time_related_parameters()
-        self.update_params()
-
-    def calculate_time_related_parameters(self):
-        dt = self.p_raw["time_delta"]
-        self.p_raw["timeon"] += dt
-        self.p_raw["ride_time"] += dt
-        self.p_raw["ride_time_total"] += dt
 
     def set_raw_param(self, param_name, value):
         self.log.debug("Setting {} to {} (raw)".format(param_name, value), extra=self.extra)
@@ -175,10 +121,6 @@ class ride_parameters():
 
     def update_params(self):
         self.update_rtc()
-        self.update_hms("ride_time")
-        self.update_hms("ride_time_total")
-        self.update_hms("timeon")
-        #self.update_param("timeon")
         self.params["utc"] = self.p_raw["utc"]
 
     def strip_end(self, param_name, suffix=None):
@@ -191,14 +133,9 @@ class ride_parameters():
                 param_name = param_name[:length]
         return param_name
 
-    def reset_ride(self):
-        self.p_raw["ride_time"] = 0.0
-
     def reset_param(self, param_name):
         self.log.debug("Resetting {}".format(param_name), extra=self.extra)
         self.p_raw[param_name] = 0
-        if param_name in ("ride_time"):
-            self.reset_ride()
 
     def update_param(self, param):
         #print("param = {}, raw value = {} format = {}".format(param, self.p_raw[param], self.p_format[param]))
@@ -226,20 +163,7 @@ class ride_parameters():
             value = "0" + format(value)
         return value
 
-    def update_hms(self, param):
-        t = divmod(int(self.p_raw[param]), 3600)
-        hrs = t[0]
-        sec = t[1]
-        t = divmod(t[1], 60)
-        mins = t[0]
-        sec = t[1]
-        hrs = self.add_zero(hrs)
-        mins = self.add_zero(mins)
-        sec = self.add_zero(sec)
-        self.params[param + "_hms"] = "{}:{}:{}".format(hrs, mins, sec)
-
     def update_rtc(self):
         # FIXME proper localisation would be nice....
         self.params["date"] = strftime("%d-%m-%Y")
         self.params["time"] = strftime("%H:%M:%S")
-        self.params["rtc"] = self.params["date"] + " " + self.params["time"]
