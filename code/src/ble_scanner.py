428fbee16922a9996f1410deda4bc6116268b482
Przemo Firszt <przemo@firszt.eu>
2018-10-27 11:21:48 +0100

    Start working on ble_scanner
    Signed-off-by: Przemo Firszt <przemo@firszt.eu>

--- code/src/ble_scanner.py
+++ code/src/ble_scanner.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 ## @package ble_scanner
 #  BLE scanner module. Provides list of BLE devices. Can be run directly printing the list of devices to the console.
-#  Uses fixed scan time (10s)
+#  Uses default scan time of 5s
 import bluepy
 import logging
 
@@ -9,7 +9,8 @@ import logging
 ## Helper class inheriting from bluepy DefaultDelegate
 class ScanDelegate(bluepy.btle.DefaultDelegate):
     def __init__(self):
-        bluepy.btle.DefaultDelegate.__init__(self)
+        # Run init for super class
+        super().__init__()
 
 #    def handleDiscovery(self, dev, isNewDev, isNewData):
 #        if isNewDev:
@@ -27,95 +28,43 @@ class ble_scanner(object):
 
     ## The constructor
     #  @param self The python object self
-    def __init__(self, occ):
-        ## @var l
+    def __init__(self):
+        ## @var log
         # System logger handle
         self.log = logging.getLogger('system')
-        ## @var occ
-        # OCC Handle
-        self.occ = occ
-        ## @var rp
-        # ride_parameters instance handle
-        self.rp = occ.rp
         ## @var scanner
         #  BLE scanner from bluepy
         self.scanner = bluepy.btle.Scanner().withDelegate(ScanDelegate())
-        ## @var dev_list_raw
-        #  List of BLE devices stored for get_dev_list function
-        self.dev_list_raw = []
+        ## @var ble_devices
+        #  List of BLE devices sorted by signal strength
+        # Example results (list of dict):
+        # [{'rss': -68, 'addr': 'fd:df:0e:4e:76:cf', 'name': 'Lezyne S&C 249', 'addr_type': 'random'}, {'rss': -71, 'addr': 'd6:90:a8:08:f0:e4', 'name': 'Tacx HRB 04741', 'addr_type': 'random'}]
+        self.ble_devices = []
 
-    ## Searches for BLE devices over 10 seconds (defaut). Stores found devices to self.dev_list_raw
+    ## Searches for BLE devices over 5 seconds (defaut). Stores found devices in self.ble_devices
     #  @param self The python object self
     #  @param timeout time that scanner should look for BLE devices
-    def scan(self, timeout=10.0):
-        ## @var devices
-        #  List of devices returned by bluepy scanner
+    def scan(self, timeout=5.0):
+        devices = []
         try:
-            devices = self.scanner.scan(timeout)
+            devices_raw = self.scanner.scan(timeout)
         except bluepy.btle.BTLEException as exception:
-            #Failed to execute mgmt cmd 'le on'
-            self.log.error("Exception {}".format(exception), extra=self.mod_name)
+            self.log.error("Exception {}".format(exception), extra=self.extra)
         else:
-            self.dev_list_raw = []
-            for dev in devices:
-                ## @var local_name
-                #  By default all devices are named "Unknown". If device reports a bane it will be used instead.
-                local_name = "Unknown"
+            devices = []
+            for dev in devices_raw:
+                local_name = 'Unknown'
                 for (adtype, desc, value) in dev.getScanData():
                     if adtype == 9:
                         local_name = value
-                self.dev_list_raw.append(dict(addr=dev.addr, name=local_name, addr_type=dev.addrType, rss=dev.rssi))
-
-    ## Returns list of BLE devices. scan function needs to be called first.
-    #  List of BLE devices stored for get_dev_list function
-    def get_dev_list(self):
-        ## @var dl
-        #  Local variable, used to sort list of BLE devices per signal strength
-        dl = sorted(self.dev_list_raw, key=lambda k: k['rss'], reverse=True)
-        return dl
-
-    def ble_scan(self):
-        self.log.debug("starting BLE scanning", extra=self.mod_name)
-        for i in range(5):
-            self.rp.set_param('ble_dev_name_' + str(i), "Scanning..")
-        self.scan()
-        for i in range(5):
-            self.rp.set_param('ble_dev_name_' + str(i), "")
-        i = 1
-        for dev in self.get_dev_list():
-            self.log.debug("BLE device found! Name:\"{}\" addr:\"{}\"".format(dev['name'], dev['addr']), extra=self.mod_name)
-            self.rp.set_param('ble_dev_name_' + str(i), dev['name'])
-            self.rp.set_param('ble_dev_addr_' + str(i), dev['addr'])
-            i += 1
-        self.log.debug("BLE scanning finished", extra=self.mod_name)
-
-    def ble_dev_helper(self, no, master):
-        if master == 'ble_hr_name':
-            dev_type = 'hr'
-        elif master == 'ble_sc_name':
-            dev_type = 'sc'
-        name = self.rp.get_param('ble_dev_name_' + str(no))
-        addr = self.rp.get_param('ble_dev_addr_' + str(no))
-        self.log.debug("Selected BLE device {} {}".format(name, addr), extra=self.mod_name)
-        self.rp.set_param("variable_value", (name, addr, dev_type))
-        self.occ.layout.ed_accept()
-
-    def ble_dev_name_1(self):
-        self.ble_dev_helper(1, self.rp.params["variable"])
-
-    def ble_dev_name_2(self):
-        self.ble_dev_helper(2, self.rp.params["variable"])
-
-    def ble_dev_name_3(self):
-        self.ble_dev_helper(3, self.rp.params["variable"])
-
-    def ble_dev_name_4(self):
-        self.ble_dev_helper(4, self.rp.params["variable"])
+                devices.append(dict(addr=dev.addr, name=local_name, addr_type=dev.addrType, rss=dev.rssi))
+        self.ble_devices = sorted(devices, key=lambda k: k['rss'], reverse=True)
 
 
-#TODO Currently doesn't work as it is OCC dependent
 if __name__ == '__main__':
     ble = ble_scanner()
+    print('Scanning for 5s')
     ble.scan()
-    for i in ble.get_dev_list():
+    print(ble.ble_devices)
+    for i in ble.ble_devices:
         print(i)
