674f4dbdbb54cf606ea70533f9c0fb87a85cc673
Przemo Firszt <przemo@firszt.eu>
2018-11-02 16:56:34 +0000

    Update gen_valid_values.sh script
    Signed-off-by: Przemo Firszt <przemo@firszt.eu>

--- gen_valid_values.sh
+++ gen_valid_values.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 echo "Registered parameters:"
-git grep register_parameter | grep -v sensors.py | sed -s "s/code\/src\///" | sed -s "s/self.s.register_parameter(\"//" | sed -s "s/\"\, self.extra\[\"module_name\"\].*//" | sed -s "s/self.sensors.register_parameter(\"//"
+git grep register_parameter | grep -v plugin_manager.py | sed -s "s/code\/src\///" | sed -s "s/self.pm.register_parameter(\"//" | sed -s "s/\"\, self.extra\[\"module_name\"\].*//" | sed -s "s/self.sensors.register_parameter(\"//" | grep -v def | grep -v 'self\.log'
 
 echo "Requested parameters:"
-git grep request_parameter | grep -v sensors.py | sed -s "s/code\/src\///" | sed -s "s/self.s.request_parameter(\"//" | sed -s "s/\"\, self.extra\[\"module_name\"\].*//"
+git grep request_parameter | grep -v plugin_manager.py | sed -s "s/code\/src\///" | sed -s "s/self.pm.request_parameter(\"//" | sed -s "s/\"\, self.extra\[\"module_name\"\].*//" | grep -v def | grep -v 'self\.log'
+
+echo "Registered input queue:"
+git grep register_input_queue | grep -v plugin_manager.py | sed -s "s/code\/src\///" | sed -s "s/self.pm.register_input_queue.*.//" | sed -s "s/\"\, self.extra\[\"module_name\"\].*//" | grep -v def | grep -v 'self\.log'
+
+echo "Registered event queue:"
+git grep register_event_queue | grep -v plugin_manager.py | sed -s "s/code\/src\///" | sed -s "s/self.pm.register_event_queue.*//" | sed -s "s/\"\, self.extra\[\"module_name\"\].*//" | grep -v def | grep -v 'self\.log'
