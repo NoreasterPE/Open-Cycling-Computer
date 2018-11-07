062163d1f7adbd341b120c2af37bd7df91c8e376
Przemo Firszt <przemo@firszt.eu>
2018-11-07 11:19:25 +0000

    Reduce weather monitor demo to bare minimum
    Signed-off-by: Przemo Firszt <przemo@firszt.eu>

--- code/weather.sh
+++ code/weather.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 PWD=$(pwd)
-CONFIG=$PWD'/config/config_weather.yaml'
-LOG_CONFIG=$PWD'/config/weather_log.yaml'
-LAYOUT=$PWD'/layouts/weather.yaml'
-FONTS=$PWD'/fonts/'
 HELPERS=$PWD'/src/helpers'
-PYTHONPATH=$HELPERS python3 src/weather.py $CONFIG $LOG_CONFIG $LAYOUT $FONTS
+PYTHONPATH=$HELPERS python3 src/weather.py
