2f38ba254d269f07186db9f805dbda64e79d13ba
Przemo Firszt <przemo@firszt.eu>
2018-11-07 10:43:21 +0000

    Fix problem with non-unique parameter names in ride_log config
    Signed-off-by: Przemo Firszt <przemo@firszt.eu>

--- code/weather.sh
+++ code/weather.sh
@@ -5,4 +5,4 @@ LOG_CONFIG=$PWD'/config/weather_log.yaml'
 LAYOUT=$PWD'/layouts/weather.yaml'
 FONTS=$PWD'/fonts/'
 HELPERS=$PWD'/src/helpers'
-PYTHONPATH=$HELPERS python3 src/occ.py $CONFIG $LOG_CONFIG $LAYOUT $FONTS
+PYTHONPATH=$HELPERS python3 src/weather.py $CONFIG $LOG_CONFIG $LAYOUT $FONTS
