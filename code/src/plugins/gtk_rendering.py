9ebd7f755491949019fe2abb3ff5bfc2d638d636
Przemo Firszt <przemo@firszt.eu>
2018-11-08 14:16:59 +0000

    gtk_rendering plugin
    Signed-off-by: Przemo Firszt <przemo@firszt.eu>

--- code/src/plugins/gtk_rendering.py
+++ code/src/plugins/gtk_rendering.py
@@ -2,6 +2,7 @@
 ## @package gtk_rendering
 #  Rendering module in GTK window
 
+import cairo
 import plugin
 import pyplum
 import time
@@ -9,8 +10,7 @@ import threading
 
 import gi
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk
-#import cairo
+from gi.repository import Gtk, Gdk
 
 
 ## Display rendering class
@@ -33,14 +33,14 @@ class gtk_rendering(plugin.plugin):
         try:
             self.width = self.pm.parameters['display_size']['value'][0]
         except KeyError:
-            self.log.critical('gtk_rendering init failed on display_size')
+            self.log.critical('gtk_rendering init failed on display_size', extra=self.extra)
             raise
         ## @var height
         #  Window/screen height
         try:
             self.height = self.pm.parameters['display_size']['value'][1]
         except KeyError:
-            self.log.critical('gtk_rendering init failed on display_size')
+            self.log.critical('gtk_rendering init failed on display_size', extra=self.extra)
             raise
         #FIXME add param to control fps/screenshot here
         ## @var running
@@ -66,35 +66,28 @@ class gtk_rendering(plugin.plugin):
     def setup_cairo(self):
         if self.width is not None and self.height is not None and not self.cairo_initialised:
             win = Gtk.Window()
-            win.set_title('Drawing Area')
-            win.connect('destroy', Gtk.main_quit)
-            win.connect('delete-event', Gtk.main_quit)
-            win.set_border_width(8)
-
-            da = Gtk.DrawingArea()
-            da.set_size_request(240, 320)
-            #da.connect('draw', draw_cb)
-            win.add(da)
+            win.set_decorated(False)
+            win.set_default_size(self.width, self.height)
+            self.drawing_area = Gtk.DrawingArea()
+            win.add(self.drawing_area)
             win.show_all()
-            myGdkWindow = da.get_window()
-            self.ctx = myGdkWindow.cairo_create()
-            #thread = threading.Thread(target=Gtk.main())
-            #thread.start()
-            #thread.join()
-            # Main cairo context
-            #self.ctx.set_source_surface(self.surface, 0, 0)
-            #self.ctx = cairo.Context(self.surface)
-            #self.ctx.set_source_rgba(0.0, 0.0, 0.0, 1.0)
-            #self.ctx.paint_with_alpha(1.0)
-            self.pm.register_cairo_context(self.extra['module_name'], self.ctx)
-            threading.Thread(target=Gtk.main).start()
+            win.set_app_paintable(True)
+            win.set_double_buffered(False)
+            # Window context
+            self.ctx = self.drawing_area.get_window().cairo_create()
+            # Window surface
+            s = self.ctx.get_target()
+            # Soft framebuffer surface
+            self.surface_buf = cairo.ImageSurface.create_similar(s, cairo.CONTENT_COLOR, self.width, self.height)
+            # Soft framebuffer context
+            self.ctx_buffer = cairo.Context(self.surface_buf)
+            self.pm.register_cairo_context(self.extra['module_name'], self.ctx_buffer)
             self.cairo_initialised = True
+            Gdk.threads_init()
+            threading.Thread(target=Gtk.main).start()
 
     def run(self):
         self.running = True
-        import cairo
-        global png_surface
-        png_surface = cairo.ImageSurface.create_from_png('images/page_0_background.png')
         while self.running:
             if (self.width is not None and
                     self.height is not None and
@@ -102,11 +95,25 @@ class gtk_rendering(plugin.plugin):
                 self.setup_cairo()
             if self.pm.render['refresh'] and not self.pm.render['hold']:
                 self.pm.render['hold'] = True
-                #call draw here
-                s = self.ctx.get_target()
-                self.ctx.set_source_surface(s, 0, 0)
-                self.ctx.fill()
                 self.pm.render['refresh'] = False
+                Gdk.threads_enter()
+                self.ctx.set_source_surface(self.surface_buf, 0, 0)
+                self.ctx.rectangle(0, 0, self.width, self.height)
+                try:
+                    self.ctx.fill()
+                except cairo.Error as e:
+                    #Exception in thread None:
+                    #Traceback (most recent call last):
+                    #  File "/usr/lib64/python3.6/threading.py", line 916, in _bootstrap_inner
+                    #    self.run()
+                    #  File "/home/przemo/software/occ/OpenCyclingComputer/Open-Cycling-Computer/code/src/plugins/gtk_rendering.py", line 102, in run
+                    #    self.ctx.fill()
+                    #cairo.Error: the target surface has been finished
+                    if e == 'the target surface has been finished':
+                        self.log.critical('The target surface no longer exist.', extra=self.extra)
+                        self.running = False
+                self.drawing_area.queue_draw()
+                Gdk.threads_leave()
                 self.pm.render['hold'] = False
                 try:
                     if self.pm.parameters['screenshot_mode']['value']:
@@ -124,18 +131,3 @@ class gtk_rendering(plugin.plugin):
 
     def __del__(self):
         self.stop()
-
-
-def draw_cb(wid, cr):
-    #    import cairo
-    #    png_surface = cairo.ImageSurface.create_from_png('images/page_0_background.png')
-    #    cr.set_source_surface(png_surface, 0, 0)
-    #    cr.rectangle(0, 0, 240, 320)
-    #    cr.fill()
-    s = cr.get_target()
-    cr.set_source_surface(s, 0, 0)
-    cr.paint()
-    #cr.set_source_surface(png_surface, 0, 0)
-    #cr.rectangle(0, 0, 240, 320)
-    #cr.fill()
-    return False
