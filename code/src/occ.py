788c7f617302eb434938c5ee53ad346b34144407
Przemo Firszt <przemo@firszt.eu>
2018-11-10 20:30:30 +0000

    Use pimoroni_lipo_shim
    Signed-off-by: Przemo Firszt <przemo@firszt.eu>

--- code/src/occ.py
+++ code/src/occ.py
@@ -119,6 +119,7 @@ if __name__ == "__main__":
                #'gtk_rendering',
                'pitft_rendering',
                'pitft_touchscreen',
+               'pimoroni_lipo_shim',
                'data_log',
                'syscalls']
     p_manager.load_plugins('plugins', plugins)
