0b392de1c650d400e9399b3570bb0f8fc6b7e67c
Przemo Firszt <przemo@firszt.eu>
2018-11-19 18:29:00 +0000

    Tidy up ble_Scanner
    Signed-off-by: Przemo Firszt <przemo@firszt.eu>

--- code/src/plugins/ble_scanner.py
+++ code/src/plugins/ble_scanner.py
@@ -34,8 +34,8 @@ class ble_scanner(plugin.plugin):
         # Run init for super class
         super().__init__()
         self.pm = pyplum.pyplum()
-        self.pm.register_parameter("ble_scan_in_progress", self.extra["module_name"], value=False)
-        self.pm.register_parameter("ble_devices", self.extra["module_name"], value=[])
+        self.pm.register_parameter("ble_scan_done", self.extra["module_name"], value=False)
+        self.pm.register_parameter("ble_scan_results", self.extra["module_name"], value=[])
         ## @var scanner
         #  BLE scanner from bluepy
         self.scanner = bluepy.btle.Scanner().withDelegate(ScanDelegate())
@@ -46,20 +46,10 @@ class ble_scanner(plugin.plugin):
         #  {'rss': -71, 'addr': 'd6:90:a8:08:f0:e4', 'name': 'Tacx HRB 04741', 'addr_type': 'random'}]
         self.ble_devices = []
 
-    def scan(self):
-        if self.pm.event_queue is not None:
-            for i in range(4):
-                self.pm.event_queue.put(('show_overlay', 'images/ol_ble_scanning.png', 1.0))
-                self.pm.event_queue.put(('show_overlay', 'images/ol_ble_scanning_0.png', 1.0))
-                self.pm.event_queue.put(('show_overlay', 'images/ol_ble_scanning_1.png', 1.0))
-                self.pm.event_queue.put(('show_overlay', 'images/ol_ble_scanning_2.png', 1.0))
-        threading.Thread(target=self._scan).start()
-
     ## Searches for BLE devices over 5 seconds (defaut). Stores found devices in self.ble_devices
     #  @param self The python object self
     #  @param timeout time that scanner should look for BLE devices
-    def _scan(self, timeout=10.0):
-        self.pm.parameters['ble_scan_in_progress']['value'] = True
+    def scan(self, timeout=4.0):
         devices = []
         try:
             devices_raw = self.scanner.scan(timeout)
@@ -68,15 +58,33 @@ class ble_scanner(plugin.plugin):
         else:
             devices = []
             for dev in devices_raw:
-                local_name = 'Unknown'
+                local_name = None
                 for (adtype, desc, value) in dev.getScanData():
                     if adtype == 9:
                         local_name = value
+                    if local_name is None:
+                        local_name = dev.addr
                 devices.append(dict(addr=dev.addr, name=local_name, addr_type=dev.addrType, rss=dev.rssi))
+        # Add null device used to disconnect currently connected device
+        # FIXME That might be conditional, dependng on currently connected device
+        # To be fixed after the code is split into ble_sc/ble_hr and ble_scanner part
+        devices.append(dict(addr=None, name='Disconnect', addr_type=None, rss=0))
         self.ble_devices = sorted(devices, key=lambda k: k['rss'], reverse=True)
-        self.pm.parameters['ble_devices']['value'] = self.ble_devices
-        self.pm.parameters['ble_devices']['time_stamp'] = time.time()
-        self.pm.parameters['ble_scan_in_progress']['value'] = False
+        self.pm.parameters['ble_scan_results']['value'] = self.ble_devices
+        self.pm.parameters['ble_scan_results']['time_stamp'] = time.time()
+        self.pm.parameters['ble_scan_done']['value'] = True
 
     def run(self):
         pass
+
+    def find_ble_device(self):
+        self.set_up_ble_scan_animation()
+        threading.Thread(target=self.scan).start()
+
+    def set_up_ble_scan_animation(self):
+        if self.pm.event_queue is not None:
+            for i in range(1):
+                self.pm.event_queue.put(('show_overlay', 'images/ol_ble_scanning.png', 1.0))
+                self.pm.event_queue.put(('show_overlay', 'images/ol_ble_scanning_0.png', 1.0))
+                self.pm.event_queue.put(('show_overlay', 'images/ol_ble_scanning_1.png', 1.0))
+                self.pm.event_queue.put(('show_overlay', 'images/ol_ble_scanning_2.png', 1.0))
