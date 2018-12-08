bf79ca39f49c4e539539fc2de1e7147c0a71fb80
Przemo Firszt <przemo@firszt.eu>
2018-12-08 16:44:02 +0000

    Move scripts to subdirectory
    Signed-off-by: Przemo Firszt <przemo@firszt.eu>

---
+++ resources/scripts/generate_doc.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+pushd ../../
+doxygen conf.doxygen
+popd
--- resources/scripts/generete_valid_values.sh
+++ resources/scripts/generete_valid_values.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+pushd ../../
+
 echo "Registered parameters:"
 git grep register_parameter | grep -v pyplum.py | sed -s "s/code\/src\///" | sed -s "s/self.pm.register_parameter(\"//" | sed -s "s/\"\, self.extra\[\"module_name\"\].*//" | sed -s "s/self.sensors.register_parameter(\"//" | grep -v def | grep -v 'self\.log'
 
@@ -11,3 +13,5 @@ git grep register_input_queue | grep -v pyplum.py | sed -s "s/code\/src\///" | s
 
 echo "Registered event queue:"
 git grep register_event_queue | grep -v pyplum.py | sed -s "s/code\/src\///" | sed -s "s/self.pm.register_event_queue.*//" | sed -s "s/\"\, self.extra\[\"module_name\"\].*//" | grep -v def | grep -v 'self\.log'
+
+popd
