05443f5336cd02ff5af0f2faf63025e6e76cb41e
Przemo Firszt <przemo@firszt.eu>
2018-12-08 16:45:39 +0000

    Formatting, typos etc
    Signed-off-by: Przemo Firszt <przemo@firszt.eu>

--- code/occ.sh
+++ code/occ.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
+# Script starting OCC
+
 PWD=$(pwd)
+
 CONFIG=$PWD'/config/config.yaml'
 RIDE_LOG_CONFIG=$PWD'/config/ride_log_config.yaml'
 LAYOUT=$PWD'/layouts/default.yaml'
 FONTS=$PWD'/fonts/'
 HELPERS=$PWD'/src/helpers'
+
 PYTHONPATH=$HELPERS python3 src/occ.py $CONFIG $RIDE_LOG_CONFIG $LAYOUT $FONTS
 
