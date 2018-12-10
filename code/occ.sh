061d6ae5d26a5204310c666db86e785f6d81d957
Przemo Firszt <przemo@firszt.eu>
2018-12-10 12:09:08 +0000

    Remove redundant PYTHONPATH for helpers
    Signed-off-by: Przemo Firszt <przemo@firszt.eu>

--- code/occ.sh
+++ code/occ.sh
@@ -7,7 +7,6 @@ CONFIG=$PWD'/config/config.yaml'
 RIDE_LOG_CONFIG=$PWD'/config/ride_log_config.yaml'
 LAYOUT=$PWD'/layouts/default.yaml'
 FONTS=$PWD'/fonts/'
-HELPERS=$PWD'/src/helpers'
 
-PYTHONPATH=$HELPERS python3 src/occ.py -c $CONFIG -d $RIDE_LOG_CONFIG -l $LAYOUT -f $FONTS
+python3 src/occ.py -c $CONFIG -d $RIDE_LOG_CONFIG -l $LAYOUT -f $FONTS
 
