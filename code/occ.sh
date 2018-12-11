346c7b081e784603afec3bcc979c5e1edde16f7f
Przemo Firszt <przemo@firszt.eu>
2018-12-11 13:06:16 +0000

    Use one-page-per-file layout
    Signed-off-by: Przemo Firszt <przemo@firszt.eu>

--- code/occ.sh
+++ code/occ.sh
@@ -5,7 +5,8 @@ PWD=$(pwd)
 
 CONFIG=$PWD'/config/config.yaml'
 RIDE_LOG_CONFIG=$PWD'/config/ride_log_config.yaml'
-LAYOUT=$PWD'/layouts/default.yaml'
+LAYOUT=$PWD'/layouts/occ/'
+#LAYOUT=$PWD'/layouts/default.yaml'
 FONTS=$PWD'/fonts/'
 
 python3 src/occ.py -c $CONFIG -d $RIDE_LOG_CONFIG -l $LAYOUT -f $FONTS
