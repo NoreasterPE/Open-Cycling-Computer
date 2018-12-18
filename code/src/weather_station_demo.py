4cfac5cf967855f74d1fe29034f5fbf20279df48
Przemo Firszt <przemo@firszt.eu>
2018-12-18 16:13:38 +0000

    Use altitude lock in weather station demo
    Signed-off-by: Przemo Firszt <przemo@firszt.eu>

--- code/src/weather_station_demo.py
+++ code/src/weather_station_demo.py
@@ -26,6 +26,8 @@ p_manager = pyplum.pyplum()
 p_manager.register_parameter("display_size", value=(240, 320))
 # Register weather station altitude. Required for MSLP pressure calculation
 p_manager.update_parameter("reference_altitude", dict(value='30', raw_unit='m'))
+# Lock altitude. Normally pressure changes are used to determine altitude change, but for weather station altitude doesn't change
+p_manager.update_parameter("altitude_lock", dict(value=True))
 p_manager.register_parameter("data_log_config", value='config/weather_log.yaml')
 p_manager.register_parameter("layout_location", None, value='layouts/weather_station_demo/')
 p_manager.register_parameter("fonts_dir", None, value='fonts/')
